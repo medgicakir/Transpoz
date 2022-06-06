f0246c86eb3d6dd53252a613a72236d9eb88bcca
medgicakir <cigdemcakir4897@gmail.com>
2022-06-06 18:53:33 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] matris = {{1, 2, 3}, {4, 5, 6},};
+        int[][] transpoz = new int[3][2];
+
+        System.out.println("Matris: ");
+        for (int i = 0; i < matris.length; i++) {
+            for (int j = 0; j < matris[i].length; j++) {
+
+                System.out.print(matris[i][j] + " ");
+
+
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Transpoz: ");
+        for (int i = 0; i < transpoz.length; i++) {
+            for (int j = 0; j < transpoz[i].length; j++) {
+                transpoz[i][j] = matris[j][i];
+                System.out.print(transpoz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
